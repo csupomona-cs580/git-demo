1321ce5711c6ca29a35f3cead730c9f34ab31ec6
Yu Sun <yu.sun.cs@gmail.com>
2017-01-18 16:51:06 -0800

    Alice's first change

--- Demo.java
+++ Demo.java
@@ -1,8 +1,9 @@
+// this is my first code change
 public class Demo {
         public int counter;
 
 	public static void main(String[] args) {
            int a = 0;
-	   int b = 0;		
+	   int b = 0;
 	}
 }
